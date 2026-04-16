8dc6403244f15b0cd9d929b27aabe8f6f4c636c5
seyefarukie77 <seyefarukie@yahoo.com>
2026-04-16 21:55:21 +0000

    Update terraform and deploy.yml

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,10 @@
 terraform {
-  required_version = ">= 1.5"
+  required_version = ">= 1.5.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~
+      version = "~> 5.0"
+    }
+  }
+}
