3f2f59ba37fb7a67ffd57063c632b894293e4d60
seyefarukie77 <seyefarukie@yahoo.com>
2026-04-23 13:12:04 +0000

    Unit Test

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from app.main import (
+    run_ddr_assessment,
+    evaluate_detect,
+    evaluate_pir_readiness,
+    MOCK_INCIDENTS,
+    CheckStatus,
+    Severity,
+    SEVERITY_RULES,
+)
+
+__all__ = [
+    "run_ddr_assessment",
+    "evaluate_detect",
+    "evaluate_pir_readiness",
+    "MOCK_INCIDENTS",
+    "CheckStatus",
+    "Severity",
+    "SEVERITY_RULES",
+]
